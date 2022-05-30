402a7b40f2d277dd55767c1d16203e898f9b522d
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-05-30 10:36:12 +0900

    formatted

--- fragment-collections/nagoya-fragments/fragments/nagoya-header/index.js
+++ fragment-collections/nagoya-fragments/fragments/nagoya-header/index.js
@@ -2,23 +2,23 @@ function insertAfter(newNode, existingNode) {
   existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
-const notificationButton = document.querySelector('#notification-btn');
+const notificationButton = document.querySelector("#notification-btn");
 let notificationLinkUrl = new URL(notificationButton.href);
-notificationLinkUrl.searchParams.append('p_p_auth', Liferay.authToken);
+notificationLinkUrl.searchParams.append("p_p_auth", Liferay.authToken);
 notificationButton.href = notificationLinkUrl.toString();
 
-const secret = document.querySelector('#secret');
+const secret = document.querySelector("#secret");
 const badge = secret.querySelector(".badge");
 
 badge.classList.add("notification-badge");
 
 insertAfter(badge, notificationButton);
 
-const contentElement = document.querySelector('#content');
+const contentElement = document.querySelector("#content");
 
 const outputsize = () => {
-fragmentElement.children[0].style.width = `${contentElement.offsetWidth}px`
-}
-outputsize()
+  fragmentElement.children[0].style.width = `${contentElement.offsetWidth}px`;
+};
+outputsize();
 
-new ResizeObserver(outputsize).observe(contentElement)
+new ResizeObserver(outputsize).observe(contentElement);
